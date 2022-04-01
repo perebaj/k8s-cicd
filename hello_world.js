bbdcf63653319c195e16b6d60fba91ddbfd6629c
Jonathan <jonathan.silva@birdie.ai>
2022-04-01 11:54:38 -0300

    :bug: image tag

--- hello_world.js
+++ hello_world.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('SERA Q VAI DAR BOM??/');
+    res.send('SERA Q ??/');
 });
 
 app.listen(PORT, HOST);
